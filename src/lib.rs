b9c4715af86863efd8cb9526cb3c27b605870780
wcrvt <cat0127.k@gmail.com>
2024-06-23 02:33:33 +0900

    add re-export module: serder

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub use cands_interface::{TCAN455xTranceiver, RxData, SIDFCONFIG, XIDFCONFIG};
 pub use cands_transport::cyphal::{CyphalMiddleware, CyphalRxFrame, CyphalRxPacketType, CRC_SIZE_BYTES};
+pub use cands_presentation::cyphal as serde;
 
 mod special_instructions;
 pub use special_instructions::digitalservo;
